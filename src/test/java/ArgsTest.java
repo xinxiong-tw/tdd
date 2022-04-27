f25df3c97b0f53bf0d8d37d896343d9c3ac9e195
xxiong <xin.xiong@thoughtworks.com>
2022-04-27 20:25:25 +0800

    pass bool option with different name

--- src/test/java/ArgsTest.java
+++ src/test/java/ArgsTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Constructor;
 import java.util.Arrays;
 
 public class ArgsTest {
@@ -17,13 +18,35 @@ public class ArgsTest {
         Assertions.assertFalse(options.logging());
     }
 
+    @Test
+    public void should_return_false_while_name_is_not_l_and_not_present() {
+        BoolOptionV options = Args.parse(BoolOptionV.class, "");
+        Assertions.assertFalse(options.isVerbose());
+    }
+
     record BoolOptions(@Option("l") boolean logging) {
     }
 
+    record BoolOptionV(@Option("v") boolean isVerbose) {
+    }
+
     private static class Args {
-        public static BoolOptions parse(Class<BoolOptions> optionsClass, String... args) {
-            boolean logging = Arrays.asList(args).contains("-l");
-            return new BoolOptions(logging);
+        public static <T> T parse(Class<T> optionsClass, String... args) {
+            Constructor<?> constructor = optionsClass.getDeclaredConstructors()[0];
+            Boolean[] params = Arrays.stream(constructor.getParameters()).map(parameter -> {
+                Option option = parameter.getAnnotation(Option.class);
+                String optionName = option.value();
+                return !Arrays.stream(args)
+                        .filter(arg -> arg.equals("-" + optionName))
+                        .findFirst()
+                        .map(String::isEmpty)
+                        .orElse(true);
+            }).toArray(Boolean[]::new);
+            try {
+                return (T) constructor.newInstance(params);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
