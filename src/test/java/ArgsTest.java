9c5a29d7b3be2c6a003645a70eb4e04281d9d5f5
xxiong <xin.xiong@thoughtworks.com>
2022-04-27 16:45:59 +0800

    setup init

---
+++ src/test/java/ArgsTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ArgsTest {
+    // -l -p 8080 -d /usr/logs
+    @Test
+    public void should_return_true_while_l_is_present() {
+        BoolOptions options = Args.parse(BoolOptions.class, "-l");
+        Assertions.assertTrue(options.logging());
+    }
+
+    @Test
+    public void should_return_false_while_l_is_not_present() {
+        BoolOptions options = Args.parse(BoolOptions.class, "");
+        Assertions.assertFalse(options.logging());
+    }
+
+    record BoolOptions(@Option("l") boolean logging) {
+    }
+
+    private static class Args {
+        public static BoolOptions parse(Class<BoolOptions> optionsClass, String... args) {
+            return new BoolOptions(true);
+        }
+    }
+}
