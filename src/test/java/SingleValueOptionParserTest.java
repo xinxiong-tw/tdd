5157583091a8006b5140dca613beb03df0e9d4b1
xxiong <xin.xiong@thoughtworks.com>
2022-04-28 22:11:46 +0800

    fix test wrong obj, SingleValueOptionParserTest should test SingleValueOptionParser

--- src/test/java/SingleValueOptionParserTest.java
+++ src/test/java/SingleValueOptionParserTest.java
@@ -1,50 +1,33 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+import java.util.function.Function;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class SingleValueOptionParserTest {
-    // single int
+
+    private final SingleValueOptionParser<Integer> intOptionParser = new SingleValueOptionParser<>(Integer::parseInt);
+
+    // single value
     @Test
     public void should_return_8080_while_give_port() {
-        IntOption option = Args.parse(IntOption.class, "-p", "8080");
-        assertEquals(option.port(), 8080);
+        assertEquals(intOptionParser.parse(List.of("-p", "8080"), "-p"), 8080);
     }
 
     @Test
     public void should_throw_error_while_no_value() {
-        assertThrows(IllegalArgumentException.class, () -> Args.parse(IntOption.class, "-p"));
+        assertThrows(IllegalArgumentException.class, () -> intOptionParser.parse(List.of("-p"), "-p"));
     }
 
     @Test
     public void should_throw_error_while_no_value_2() {
-        assertThrows(IllegalArgumentException.class, () -> Args.parse(IntOption.class, "-p", "-l"));
+        assertThrows(IllegalArgumentException.class, () -> intOptionParser.parse(List.of("-p", "-l"), "-p"));
     }
 
     @Test
     public void should_throw_error_while_extra_value() {
-        assertThrows(IllegalArgumentException.class, () -> Args.parse(IntOption.class, "-p", "8080", "8888"));
-    }
-
-    record IntOption(@Option("p") int port) {
-    }
-
-    // single string
-    @Test
-    public void should_return_log_file_path_while_give_directory() {
-        StringOption option = Args.parse(StringOption.class, "-d", "/usr/logs");
-        assertEquals(option.directory(), "/usr/logs");
-    }
-
-    @Test
-    public void should_throw_error_while_no_value_for_directory() {
-        assertThrows(IllegalArgumentException.class, () -> Args.parse(StringOption.class, "-d", "-p", "8080"));
-    }
-
-    @Test
-    public void should_throw_error_while_no_value_for_directory_2() {
-        assertThrows(IllegalArgumentException.class, () -> Args.parse(StringOption.class, "-d", "-l"));
-    }
-
-    record StringOption(@Option("d") String directory) {
+        assertThrows(IllegalArgumentException.class, () -> intOptionParser.parse(List.of("-p", "8080", "8888"), "-p"));
     }
 }
