9d91b171f8c3d802f492ee8bc2bce9f88cccb2af
xxiong <xin.xiong@thoughtworks.com>
2022-04-28 22:05:35 +0800

    fix test wrong obj, BoolOptionParserTest should test BoolOptionParser

--- src/test/java/BoolOptionParserTest.java
+++ src/test/java/BoolOptionParserTest.java
@@ -1,30 +1,32 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BoolOptionParserTest {
+
+    private final BoolOptionParser parser = new BoolOptionParser();
+
     // single bool
     @Test
     public void should_return_true_while_l_is_present() {
-        BoolOptions options = Args.parse(BoolOptions.class, "-l");
-        assertTrue(options.logging());
+        assertTrue(parser.parse(List.of("-l"), "-l"));
     }
 
     @Test
     public void should_return_false_while_l_is_not_present() {
-        BoolOptions options = Args.parse(BoolOptions.class, "");
-        assertFalse(options.logging());
+        assertFalse(parser.parse(List.of(), "-l"));
     }
 
     @Test
     public void should_return_false_while_name_is_not_l_and_not_present() {
-        BoolOptionV options = Args.parse(BoolOptionV.class, "");
-        assertFalse(options.isVerbose());
+        assertFalse(parser.parse(List.of(), "-v"));
     }
 
     @Test
     public void should_throw_error_while_get_value() {
-        assertThrows(IllegalArgumentException.class, () -> Args.parse(BoolOptions.class, "-l", "p"));
+        assertThrows(IllegalArgumentException.class, () -> parser.parse(List.of("-l", "p"), "-l"));
     }
 
     record BoolOptions(@Option("l") boolean logging) {
