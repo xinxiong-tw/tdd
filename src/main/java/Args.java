06fca58772c4bc886072580c73af52387e2dc544
xxiong <xin.xiong@thoughtworks.com>
2022-04-29 16:47:13 +0800

    setup map style args

--- src/main/java/Args.java
+++ src/main/java/Args.java
@@ -1,60 +1,30 @@
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Parameter;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
-import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 class Args<T> {
 
-    private final Map<Class<?>, OptionParser<?>> parsers;
-    private final Class<T> optionsClass;
-
-    public Args(Map<Class<?>, OptionParser<?>> parsers, Class<T> optionClass) {
-        this.parsers = parsers;
-        this.optionsClass = optionClass;
-    }
-
-    public Args(Class<T> optionsClass) {
-        this(PARSERS, optionsClass);
-    }
-
-    public T parse(String... args) {
-        Constructor<?> constructor = optionsClass.getDeclaredConstructors()[0];
-        Object[] params = Arrays.stream(constructor.getParameters())
-                .map(parameter -> parseOption(parameter, List.of(args)))
-                .toArray();
-        try {
-            return (T) constructor.newInstance(params);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static final Map<Class<?>, OptionParser<?>> PARSERS = Map.of(
-            int.class, OptionParser.unary(0, Integer::parseInt),
-            String.class, OptionParser.unary("", Function.identity()),
-            boolean.class, OptionParser.bool(),
-            String[].class, OptionParser.list(String[]::new, Function.identity()),
-            Integer[].class, OptionParser.list(Integer[]::new, Integer::parseInt)
-    );
-
-    private Object parseOption(Parameter parameter, List<String> args) {
-        Class<?> optionType = parameter.getType();
-        Option option = parameter.getAnnotation(Option.class);
-        if (option == null) {
-            throw new IllegalArgumentException(parameter.getName() + " must have a option annotation");
-        }
-        String optionName = "-" + option.value();
-        OptionParser<?> parser = getOptionParser(optionType, parsers);
-        return Optional.ofNullable(parser)
-                .map(p -> p.parse(args, optionName))
-                .orElseThrow(UnsupportedOperationException::new);
-    }
-
-    private OptionParser<?> getOptionParser(Class<?> optionType, Map<Class<?>, OptionParser<?>> parsers) {
-        return parsers.get(optionType);
+    public static Map<String, String[]> parse(List<String> arguments) {
+        HashMap<String, List<String>> hashMap = new HashMap<>();
+        final String[] optionName = new String[1];
+        arguments.forEach(argument -> {
+            String regex = "^-([a-zA-Z-]+)$";
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(argument);
+            if (matcher.matches()) {
+                optionName[0] = matcher.group(1);
+                hashMap.put(optionName[0], new ArrayList<>());
+            } else if (!optionName[0].isEmpty()) {
+                List<String> values = hashMap.get(optionName[0]);
+                values.add(argument);
+                hashMap.put(optionName[0], values);
+            }
+        });
+        return hashMap.entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, e -> e.getValue().toArray(String[]::new)));
     }
 
 }
--- src/test/java/ArgsTest.java
+++ src/test/java/ArgsTest.java
@@ -1,51 +1,19 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ArgsTest {
     // -l -p 8080 -d /usr/logs
     @Test
-    public void should_parse_multiple_options_one_time() {
-        LogOptions options = new Args<>(LogOptions.class).parse("-l", "-p", "8080", "-d", "/usr/logs");
-        assertTrue(options.logging());
-        assertEquals(options.port(), 8080);
-        assertEquals(options.directory, "/usr/logs");
+    public void should_return_map_after_parse_arguments() {
+
+        Map<String, String[]> map = Args.parse(List.of("-p", "8080", "-l", "-d", "/usr/log"));
+
+        assertArrayEquals(map.get("p"), new String[] {"8080"});
+        assertArrayEquals(map.get("l"), new String[] {});
+        assertArrayEquals(map.get("d"), new String[] {"/usr/log"});
     }
-
-    record LogOptions(@Option("l") boolean logging, @Option("p") int port, @Option("d") String directory) {
-    }
-
-    @Test
-    public void should_throw_error_while_no_option_annotation() {
-        assertThrows(IllegalArgumentException.class, () -> new Args<>(NoOptionOptions.class).parse("-l"));
-    }
-
-    record NoOptionOptions(boolean logging) {
-    }
-
-
-    @Test
-    public void should_support_string_list_options() {
-        StringListOptions options = new Args<>(StringListOptions.class).parse("-g", "this", "is");
-        assertArrayEquals(options.names(), new String[] {"this", "is"});
-    }
-
-    record StringListOptions(@Option("g") String[] names) {
-    }
-
-    @Test
-    public void should_support_int_list_options() {
-        IntListOptions options = new Args<>(IntListOptions.class).parse("-g", "1", "2");
-        assertArrayEquals(options.flags(), new Integer[]{1, 2});
-    }
-
-    @Test
-    public void should_support_negative_int_list_options() {
-        IntListOptions options = new Args<>(IntListOptions.class).parse("-g", "-1", "-2");
-        assertArrayEquals(options.flags(), new Integer[]{-1, -2});
-    }
-
-    record IntListOptions(@Option("g") Integer[] flags) {
-    }
-
 }
