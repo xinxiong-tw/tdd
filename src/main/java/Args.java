ba6c0dd90cc1fc37aab3d434ef8c30328f5c8486
xxiong <xin.xiong@thoughtworks.com>
2022-04-29 11:11:54 +0800

    use class style

--- src/main/java/Args.java
+++ src/main/java/Args.java
@@ -6,28 +6,30 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.function.Function;
 
-class Args {
-    public static <T> T parse(Class<T> optionsClass, String... args) {
+class Args<T> {
+
+    private final Map<Class<?>, OptionParser<?>> parsers;
+    private final Class<T> optionsClass;
+
+    public Args(Map<Class<?>, OptionParser<?>> parsers, Class<T> optionClass) {
+        this.parsers = parsers;
+        this.optionsClass = optionClass;
+    }
+
+    public Args(Class<T> optionsClass) {
+        this(PARSERS, optionsClass);
+    }
+
+    public T parse(String... args) {
         Constructor<?> constructor = optionsClass.getDeclaredConstructors()[0];
-        Object[] params = Arrays.stream(constructor.getParameters()).map(parameter -> parseOption(parameter, List.of(args))).toArray();
+        Object[] params = Arrays.stream(constructor.getParameters())
+                .map(parameter -> parseOption(parameter, List.of(args)))
+                .toArray();
         try {
             return (T) constructor.newInstance(params);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-    }
-
-    private static Object parseOption(Parameter parameter, List<String> args) {
-        Class<?> optionType = parameter.getType();
-        Option option = parameter.getAnnotation(Option.class);
-        if (option == null) {
-            throw new IllegalArgumentException(parameter.getName() + " must have a option annotation");
-        }
-        String optionName = "-" + option.value();
-        OptionParser<?> parser = getOptionParser(optionType);
-        return Optional.ofNullable(parser)
-                .map(p -> p.parse(args, optionName))
-                .orElseThrow(UnsupportedOperationException::new);
     }
 
     private static final Map<Class<?>, OptionParser<?>> PARSERS = Map.of(
@@ -38,8 +40,21 @@ class Args {
             Integer[].class, OptionParser.list(Integer[]::new, Integer::parseInt)
     );
 
-    private static OptionParser<?> getOptionParser(Class<?> optionType) {
-        return PARSERS.get(optionType);
+    private Object parseOption(Parameter parameter, List<String> args) {
+        Class<?> optionType = parameter.getType();
+        Option option = parameter.getAnnotation(Option.class);
+        if (option == null) {
+            throw new IllegalArgumentException(parameter.getName() + " must have a option annotation");
+        }
+        String optionName = "-" + option.value();
+        OptionParser<?> parser = getOptionParser(optionType, parsers);
+        return Optional.ofNullable(parser)
+                .map(p -> p.parse(args, optionName))
+                .orElseThrow(UnsupportedOperationException::new);
+    }
+
+    private OptionParser<?> getOptionParser(Class<?> optionType, Map<Class<?>, OptionParser<?>> parsers) {
+        return parsers.get(optionType);
     }
 
 }
--- src/test/java/ArgsTest.java
+++ src/test/java/ArgsTest.java
@@ -6,7 +6,7 @@ public class ArgsTest {
     // -l -p 8080 -d /usr/logs
     @Test
     public void should_parse_multiple_options_one_time() {
-        LogOptions options = Args.parse(LogOptions.class, "-l", "-p", "8080", "-d", "/usr/logs");
+        LogOptions options = new Args<>(LogOptions.class).parse("-l", "-p", "8080", "-d", "/usr/logs");
         assertTrue(options.logging());
         assertEquals(options.port(), 8080);
         assertEquals(options.directory, "/usr/logs");
@@ -17,7 +17,7 @@ public class ArgsTest {
 
     @Test
     public void should_throw_error_while_no_option_annotation() {
-        assertThrows(IllegalArgumentException.class, () -> Args.parse(NoOptionOptions.class, "-l"));
+        assertThrows(IllegalArgumentException.class, () -> new Args<>(NoOptionOptions.class).parse("-l"));
     }
 
     record NoOptionOptions(boolean logging) {
@@ -26,7 +26,7 @@ public class ArgsTest {
 
     @Test
     public void should_support_string_list_options() {
-        StringListOptions options = Args.parse(StringListOptions.class, "-g", "this", "is");
+        StringListOptions options = new Args<>(StringListOptions.class).parse("-g", "this", "is");
         assertArrayEquals(options.names(), new String[] {"this", "is"});
     }
 
@@ -35,13 +35,13 @@ public class ArgsTest {
 
     @Test
     public void should_support_int_list_options() {
-        IntListOptions options = Args.parse(IntListOptions.class, "-g", "1", "2");
+        IntListOptions options = new Args<>(IntListOptions.class).parse("-g", "1", "2");
         assertArrayEquals(options.flags(), new Integer[]{1, 2});
     }
 
     @Test
     public void should_support_negative_int_list_options() {
-        IntListOptions options = Args.parse(IntListOptions.class, "-g", "-1", "-2");
+        IntListOptions options = new Args<>(IntListOptions.class).parse("-g", "-1", "-2");
         assertArrayEquals(options.flags(), new Integer[]{-1, -2});
     }
 
